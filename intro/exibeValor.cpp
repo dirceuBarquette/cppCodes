8170a026ccd8f526c3ac2e294de40d1ff13fbb80
Dirceu Barquette <dirceu.barquette@gmail.com>
2025-09-19 20:16:28 -0300

    Introdução: primeiros códigos em c++

---
+++ intro/exibeValor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int valor;
+   cout << "Digite um valor: ";
+   cin >> valor;
+
+   cout << "O valor digitado foi " << valor << endl;
+
+   return 0;
+}
